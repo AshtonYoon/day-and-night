44e88fcd2b81c727a1869053f1d53d9e62e4b757
Ashton <y109384@naver.com>
2021-03-13 22:59:52 +0900

    new scene, quick settings

--- app/src/test/java/com/example/themeswitcher/ExampleUnitTest.kt
+++ app/src/test/java/com/example/themeswitcher/ExampleUnitTest.kt
@@ -14,4 +14,25 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun safeEnum(){
+        val theme = Theme.NoTheme
+
+        val num = when(theme){
+            Theme.Light -> 0
+            Theme.Dark -> 1
+            else -> 2
+        }.safe()
+
+        print(num)
+    }
+
+    private fun <T> T.safe() = this
+
+    enum class Theme {
+        Light,
+        Dark,
+        NoTheme
+    }
 }
